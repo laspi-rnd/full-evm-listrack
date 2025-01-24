d296f0ea5fc748697a02f74d57f10ddbca775141
henrique-videira <henriquecvideira@gmail.com>
2025-01-24 19:03:11 -0300

    save ufrj chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,53 +13,53 @@ require ("hardhat-switch-network");
     hardhat: {
       gas: "auto",
     },
+    mainnet: {
+      url: 'http://127.0.0.1:8253',
+      accounts: 
+      [ 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', // owner
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', // alice
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f']  // mike
+    },
+    alien: {  // moonbeam
+      url: 'https://rpc.api.moonbase.moonbeam.network',
+      chainId: 1287,
+      accounts: 
+      [ 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', // owner
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', // alice
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f']  // mike
+
+  },
     sepolia: {
       //url: '${process.env.API_URL}',
       url: 'https://sepolia.infura.io/v3/bdbe3f664e2c4fe09240e7c3cffa1f56',
       chainId: 11155111,
       accounts: 
-      [ '64fcd5bfca4beb158d341ea8729f3572409cd82b3dc8ee938747a3b109bf037e', // owner
-        '95b4154d870c7f4d8be947b20c19eeec9ad5a6fc00fe9c2936fcf06c4149309f', // alice
-        '355138760889ceb11307c098bad2da2414f57e580aa92c92295a196d96f985ef', // bob
-        '3fc855b6cb5bb052c72bab7600dd5b4080c8f2fa0e331be242d2fb787719f018', // charlie
-        'ba15b4f8659e6dbcba29e6ad21483f742d397a90ee9c88ab98bf89f88a58bebd', // debbie
-        '21b07198907e720575b9590478110904c8788a3b508063cae003f0063e565827', // eva
-        '2214a90679770647740e88a649c92f50a428f6fd2264820ecff0a0bb18b8d3b1'] // mike
+      [ 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', // owner
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', // alice
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f']  // mike
     },
     moonbeam: {
       url: 'https://rpc.api.moonbase.moonbeam.network',
       chainId: 1287,
       accounts: 
-      [ '64fcd5bfca4beb158d341ea8729f3572409cd82b3dc8ee938747a3b109bf037e', // owner
-        '95b4154d870c7f4d8be947b20c19eeec9ad5a6fc00fe9c2936fcf06c4149309f', // alice
-        '355138760889ceb11307c098bad2da2414f57e580aa92c92295a196d96f985ef', // bob
-         '3fc855b6cb5bb052c72bab7600dd5b4080c8f2fa0e331be242d2fb787719f018', // charlie
-        'ba15b4f8659e6dbcba29e6ad21483f742d397a90ee9c88ab98bf89f88a58bebd', // debbie
-        '21b07198907e720575b9590478110904c8788a3b508063cae003f0063e565827', // eva
-        '2214a90679770647740e88a649c92f50a428f6fd2264820ecff0a0bb18b8d3b1'] // mike
+      [ 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', // owner
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', // alice
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f']  // mike
 
   },
     alien_ufrj: {
       url: 'http://172.16.239.4:8545',
       accounts: 
-      [ '5391f21042f4de6f8b099ef78fcf313a6db49495307c83f49dada171641239b8',
-        '95b4154d870c7f4d8be947b20c19eeec9ad5a6fc00fe9c2936fcf06c4149309f',
-        '355138760889ceb11307c098bad2da2414f57e580aa92c92295a196d96f985ef',
-        '3fc855b6cb5bb052c72bab7600dd5b4080c8f2fa0e331be242d2fb787719f018',
-        'ba15b4f8659e6dbcba29e6ad21483f742d397a90ee9c88ab98bf89f88a58bebd',
-        '21b07198907e720575b9590478110904c8788a3b508063cae003f0063e565827',
-        '2214a90679770647740e88a649c92f50a428f6fd2264820ecff0a0bb18b8d3b1'] 
+      [ 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', // owner
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', // alice
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f']  // mike 
     },
     drex_ufrj: {
-      url: 'http://172.16.238.9:8545',
+      url: 'http://127.0.0.1:8253',
       accounts: 
-      [ '5391f21042f4de6f8b099ef78fcf313a6db49495307c83f49dada171641239b8',
-        '95b4154d870c7f4d8be947b20c19eeec9ad5a6fc00fe9c2936fcf06c4149309f',
-        '355138760889ceb11307c098bad2da2414f57e580aa92c92295a196d96f985ef',
-        '3fc855b6cb5bb052c72bab7600dd5b4080c8f2fa0e331be242d2fb787719f018',
-        'ba15b4f8659e6dbcba29e6ad21483f742d397a90ee9c88ab98bf89f88a58bebd',
-        '21b07198907e720575b9590478110904c8788a3b508063cae003f0063e565827',
-        '2214a90679770647740e88a649c92f50a428f6fd2264820ecff0a0bb18b8d3b1'] 
+      [ 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3', // owner
+        '8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63', // alice
+        'ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f']  // mike
     }
   }
 };
---
+++ test/IB_Deploying Contracts in Chains.js
@@ -0,0 +1,121 @@
+const {expect} = require("chai");
+const { ethers } = require("hardhat");
+const { provider } = ethers.getDefaultProvider();
+
+const timeSlotDrex = 20; // in seconds
+const drexLeg = 2; // in time slot
+const alienConfirmation = 3*drexLeg; // in time slot
+const alienExpiration = 2*alienConfirmation; // in time slot
+
+//import {hre} from "hardhat/types"
+const {loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
+const {time} = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("Reverts Tests with Great Volume of Tx | Automatic Revert by Merkle Contract", function() {
+
+  it ('Should Deploy Contracts Properly', async function () {
+
+  await hre.switchNetwork('mainnet');
+
+  const [owner,alice,mike] = await ethers.getSigners();
+  
+  const drexSigners = [alice,mike];
+  const alienSigners = [alice,mike];
+  
+  const merkleSigners = [owner,alice,mike];
+  
+  const equalbalance = (10**15); // 18 is the number of decimals
+  
+  const drexTokenFactory = await ethers.getContractFactory("DrexToken");
+  //const drexToken = await ethers.deployContract("DrexToken");
+  const drexToken = drexTokenFactory.attach('0x7CfdA9c2aE901bA78897594070Db4Dbe264023ba');
+
+  console.log ('#######################');
+  console.log (drexToken.target);
+  
+  // the below code is the first token transfer to users
+ // for (let i = 0; i < drexSigners.length; i++) {
+ // await drexToken.transfer(drexSigners[i].address, equalbalance);
+ // }
+
+  /* Setup for alienToken */
+
+  await hre.switchNetwork('alien');
+
+  //
+
+    const alienTokenFactory = await ethers.getContractFactory("AlienToken");
+    //const alienToken = await ethers.deployContract("AlienToken");
+    const alienToken = alienTokenFactory.attach('0x9840EeC2eb6DA5F07088DA094dD5DCF4CC73094f');
+
+  console.log ('#######################');
+  console.log (alienToken.target);
+  
+  // Below is the Alien Token transfer to users
+  
+  for (let i = 0; i < alienSigners.length; i++) {
+    console.log (alienSigners[i].address);
+  await alienToken.connect(owner).transfer(alienSigners[i].address, equalbalance);
+  }
+  
+  
+  /* Setup for Merkle Contract */
+  await hre.switchNetwork('mainnet');
+
+  const merkleFactory = await ethers.getContractFactory("MerkleContract");
+  
+  const merkleContract 
+  = await merkleFactory.deploy([owner.address,alice.address,mike.address],[2,1,1],0);
+
+// const merkleContract = merkleFactory.attach('0x03898DAd231A2B9D9eCA58124Ab9e247AFb02bC8');
+
+    console.log ('#######################');
+    console.log (merkleContract.target);
+  
+  /* Setup for Listrack Contract */  
+  const factoryListrack = await ethers.getContractFactory("DrexListrack");
+  const Listrack = await factoryListrack.deploy(timeSlotDrex,drexLeg,
+                                alienConfirmation,alienExpiration);
+  //const Listrack = factoryListrack.attach('0x6748F5a811c6A4eC96590E15d7c77f828a0B8CDc');
+
+    console.log ('#######################');
+    console.log (Listrack.target);
+
+  
+  await hre.switchNetwork('alien');
+  /* Setup for AlienListrack Contract */
+  const factoryAlienListrack = await ethers.getContractFactory("AlienListrack");
+  
+  const alienListrack = await factoryAlienListrack.deploy(alienToken,
+                                          alienConfirmation*timeSlotDrex, // this is the time slot in seconds
+                                          alienExpiration*timeSlotDrex);  // for alien listrack 
+
+// const alienListrack = factoryAlienListrack.attach('0xC28a8b2e9704D8B5559F2d91b862409e2739c7d4');
+                                          
+console.log ('#######################');
+console.log (alienListrack.target);
+
+
+await hre.switchNetwork('mainnet');
+// Approve Listrack to spend DrexToken
+for (let i = 0; i < drexSigners.length; i++) {
+    await (drexToken.connect(drexSigners[i]).
+                approve(Listrack, equalbalance));
+    console.log ("approval emitted");
+    }
+    
+    await hre.switchNetwork('alien');
+    // Approve Alien Listrack to spend DrexToken
+    for (let i = 0; i < alienSigners.length; i++) {
+    await (alienToken.connect(alienSigners[i]).
+                approve(alienListrack, equalbalance));
+                console.log ("approval emitted");
+    } 
+
+    console.log ("** End **");
+    console.log ("** End  **");
+});
+  
+   
+});
